186a3b4182d7cbe13697f3b93c9b8da70121ddd6
jorenham <jhammudoglu@gmail.com>
2022-03-20 21:38:38 +0100

    promise tests for .then, .catch and .finally_

--- tests/test_promises.py
+++ tests/test_promises.py
@@ -77,4 +77,88 @@ async def test_already_rejected_no_exception():
 
 async def test_already_rejected_base_exception():
     with pytest.raises(TypeError):
+        # noinspection PyAsyncCall
         Promise.reject(asyncio.CancelledError('spam'))
+
+
+async def test_then():
+    fut1 = asyncio_utils.create_future()
+    fut2 = asyncio_utils.create_future()
+
+    p1 = Promise(fut1)
+
+    async def _then(_res1):
+        _res2 = await fut2
+        return _res1, _res2
+
+    p2 = p1.then(_then)
+
+    assert p1._state == 'pending'
+    assert p2._state == 'pending'
+
+    fut1.set_result('spam')
+    await p1
+
+    assert p1._state == 'fulfilled'
+    assert p2._state == 'pending'
+
+    fut2.set_result('ham')
+    await p2
+
+    assert p1._state == 'fulfilled'
+    assert p2._state == 'fulfilled'
+
+    res1 = await p1
+    res2 = await p2
+    assert res1 == 'spam'
+    assert res2 == (res1, 'ham')
+
+
+async def test_catch():
+    fut = asyncio_utils.create_future()
+
+    async def _then(_):
+        return 'fail'
+
+    async def _catch(exc):
+        if isinstance(exc, ValueError):
+            return exc.args[0] if exc.args else None
+        raise
+
+    p = Promise(fut).then(_then).catch(_catch)
+
+    assert p._state == 'pending'
+    fut.set_exception(ValueError('eggs'))
+
+    res = await p
+    assert res == 'eggs'
+
+
+async def test_finally():
+    fut_resolve = asyncio_utils.create_future()
+    fut_reject = asyncio_utils.create_future()
+
+    calls = []
+
+    async def finally_():
+        calls.append(True)
+
+    p_resolve = Promise(fut_resolve).finally_(finally_)
+    p_reject = Promise(fut_reject).finally_(finally_)
+
+    assert p_resolve._state == 'pending'
+    assert len(calls) == 0
+
+    fut_resolve.set_result(42)
+    res = await p_resolve
+
+    assert res == 42
+    assert len(calls) == 1
+
+    assert p_reject._state == 'pending'
+
+    fut_reject.set_exception(ZeroDivisionError)
+    with pytest.raises(ZeroDivisionError):
+        await p_reject
+
+    assert len(calls) == 2
